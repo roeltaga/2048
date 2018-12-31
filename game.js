c22e2b9950d21cf609740a5f32e37a94e55552f4
Roel Taga <roeltaga@gmail.com>
2018-12-31 22:18:50 +0100

    Add files via upload

---
+++ game.js
@@ -0,0 +1,13 @@
+window.onload = function() {
+
+    document.getElementById("001").innerHTML = 1024;
+    //document.getElementById("01").style.backgroundColor = "#eee4da"; //color for number 2
+
+
+    let x = document.getElementById("001");
+    if (x.innerHTML = 1024) {
+        document.getElementById("01").style.backgroundColor = "#eee4da";
+    }
+
+
+}
